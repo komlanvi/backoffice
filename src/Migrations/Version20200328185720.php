0ca63096fe0ae2a877687394ebcc928d59118f17
komlanvi <doncredas@gmail.com>
2020-03-28 20:00:19 +0100

    Update migrations.

--- src/Migrations/Version20200328185720.php
+++ src/Migrations/Version20200328185720.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200326001039 extends AbstractMigration
+final class Version20200328185720 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,6 @@ final class Version20200326001039 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE user (id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', first_name VARCHAR(255) DEFAULT NULL, last_name VARCHAR(255) DEFAULT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE video (id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', description VARCHAR(255) NOT NULL, inserted_at DATETIME NOT NULL, file_name VARCHAR(255) NOT NULL, size INT DEFAULT NULL, original_name VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE audio (id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', description VARCHAR(255) NOT NULL, inserted_at DATETIME NOT NULL, file_name VARCHAR(255) NOT NULL, size INT DEFAULT NULL, original_name VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
@@ -32,7 +31,6 @@ final class Version20200326001039 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE video');
         $this->addSql('DROP TABLE audio');
     }
